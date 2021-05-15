8328aa6cc2939e890a85bab7783be59dcfb29dc5
Yash-Singh1 <saiansh2525@gmail.com>
2021-05-15 15:12:13 -0700

    Correct require path

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 (function (mod) {
   if (typeof exports == 'object' && typeof module == 'object')
     // CommonJS
-    mod(require('../../lib/codemirror'));
+    mod(require('../codemirror/lib/codemirror'));
   else if (typeof define == 'function' && define.amd)
     // AMD
-    define(['../../lib/codemirror'], mod);
+    define(['../codemirror/lib/codemirror'], mod);
   // Plain browser env
   else mod(CodeMirror);
 })(function (CodeMirror) {
